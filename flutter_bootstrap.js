b988f431e8dc670d0071e6681d0105c25a44f5d9
keensta <andi_keen6@hotmail.com>
2026-02-07 23:13:39 +0000

    Complete UI overhall to work with mobiles better!

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1976125313"
+    serviceWorkerVersion: "640904298"
   }
 });
