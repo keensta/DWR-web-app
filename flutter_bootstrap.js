6ea57d3d0ee6499e97a7b0686162947e7776ce71
keensta <andi_keen6@hotmail.com>
2026-06-26 19:58:20 +0100

    whitescreen fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -36,9 +36,9 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2700903705" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "522292352" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
-!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="44b08ff7-bcf9-53df-bb85-506a376f3099")}catch(e){}}();
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="1426e6e9-414d-5ad6-9816-212f62f8320c")}catch(e){}}();
 //# sourceMappingURL=flutter.js.map
-//# debugId=44b08ff7-bcf9-53df-bb85-506a376f3099
+//# debugId=1426e6e9-414d-5ad6-9816-212f62f8320c
