92c111508224a571b40bdfd5f1d5a67ec71900b7
keensta <andi_keen6@hotmail.com>
2026-07-19 18:56:15 +0100

    Discord linking for Public events

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -36,9 +36,9 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "684636101" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1831785676" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
-!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="cf26ae65-8dc4-5742-8c0d-4e616973c971")}catch(e){}}();
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="0da2cc4e-f470-598b-9a37-2f93dc40d267")}catch(e){}}();
 //# sourceMappingURL=flutter.js.map
-//# debugId=cf26ae65-8dc4-5742-8c0d-4e616973c971
+//# debugId=0da2cc4e-f470-598b-9a37-2f93dc40d267
